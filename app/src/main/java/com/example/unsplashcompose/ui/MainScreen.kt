0e674fbee328da8f407264667df13850010b9f1f
Alina Poddubskaya <poddubskaya@surfstudio.ru>
2023-04-27 08:47:20 +0400

    Display list of mock images

--- app/src/main/java/com/example/unsplashcompose/ui/MainScreen.kt
+++ app/src/main/java/com/example/unsplashcompose/ui/MainScreen.kt
@@ -2,24 +2,28 @@ package com.example.unsplashcompose.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.unsplashcompose.MainViewModel
-import com.example.unsplashcompose.ui.compoments.UnsplashImage
+import com.example.unsplashcompose.ui.compoments.UnsplashImageList
 
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel = viewModel(),
 ) {
-    val image by viewModel.imageToDisplay.collectAsState()
+    val images by viewModel.images.collectAsState()
     Box(
-        modifier = modifier.padding(16.dp)
+        modifier = modifier
+            .padding(top = 16.dp, start = 20.dp, end = 20.dp)
+            .clip(RoundedCornerShape(topEnd = 12.dp, topStart = 12.dp))
     ) {
-        UnsplashImage(image = image)
+        UnsplashImageList(images = images)
     }
 }
--- app/src/main/java/com/example/unsplashcompose/ui/compoments/CommonUI.kt
+++ app/src/main/java/com/example/unsplashcompose/ui/compoments/CommonUI.kt
@@ -1,17 +1,38 @@
 package com.example.unsplashcompose.ui.compoments
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.unsplashcompose.R
 import com.example.unsplashcompose.data.UnsplashImage
 
 @Composable
-fun UnsplashImage(image: UnsplashImage) {
+fun UnsplashImage(modifier: Modifier = Modifier, image: UnsplashImage) {
     AsyncImage(
+        modifier = modifier,
         model = image.url,
         contentDescription = "",
         placeholder = painterResource(id = R.drawable.image_placeholder),
         error = painterResource(id = R.drawable.image_error)
     )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun UnsplashImageList(modifier: Modifier = Modifier, images: List<UnsplashImage>) {
+    LazyVerticalStaggeredGrid(
+        modifier = modifier,
+        columns = StaggeredGridCells.Fixed(2),
+        verticalItemSpacing = 8.dp,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(images) { UnsplashImage(modifier = modifier, image = it) }
+    }
 }
